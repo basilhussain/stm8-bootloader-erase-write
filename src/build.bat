69242f54d7fd34549be553169cedf04cddf43c68
Georg Icking-Konert <icking@onlinehome.de>
2020-12-02 07:19:21 +0100

    replaced \ by / for Posix
    replaced \ by / for Posix (don't know if it works under Windows)

--- src/build.bat
+++ src/build.bat
@@ -1,4 +1,8 @@
 @echo off
+
+REM compile sources
 sdcc -mstm8 --opt-code-size --max-allocs-per-node 25000 -DVER_128K -o obj\ -c erase.c
 sdcc -mstm8 --opt-code-size --max-allocs-per-node 25000 -DVER_128K -o obj\ -c write.c
+
+REM link sources
 sdldstm8 -n -f link\common.lk -f link\ver_128k_2.4.lk
