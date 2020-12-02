1fc15167147abbe9b51c6640ceff5aa656930e7d
Basil Hussain <basil@stasisleak.uk>
2020-12-02 06:49:09 +0000

    Update build batch file to create bin+obj directories

--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,9 @@
 @echo off
 
+REM create required directories, if required
+mkdir obj
+mkdir bin
+
 REM compile sources
 sdcc -mstm8 --opt-code-size --max-allocs-per-node 25000 -DVER_128K -o obj\ -c erase.c
 sdcc -mstm8 --opt-code-size --max-allocs-per-node 25000 -DVER_128K -o obj\ -c write.c
