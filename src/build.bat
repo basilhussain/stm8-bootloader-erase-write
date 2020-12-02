0e5bb65103a2b118c8316da3cbea4e4ccc5e5850
Basil Hussain <basil@stasisleak.uk>
2020-12-02 05:09:28 +0000

    Compilation changes
    - Higher optimisation level.
    - Move code segment specifiers to source file pragmas.
    - Make main erase/write routines callee-saves (via pragmas), as ROM code cannot be expected to save registers.

--- src/build.bat
+++ src/build.bat
@@ -1,4 +1,4 @@
 @echo off
-sdcc -mstm8 --opt-code-size --codeseg ERASE_SEG -DVER_128K -o obj\ -c erase.c
-sdcc -mstm8 --opt-code-size --codeseg WRITE_SEG -DVER_128K -o obj\ -c write.c
+sdcc -mstm8 --opt-code-size --max-allocs-per-node 25000 -DVER_128K -o obj\ -c erase.c
+sdcc -mstm8 --opt-code-size --max-allocs-per-node 25000 -DVER_128K -o obj\ -c write.c
 sdldstm8 -n -f link\common.lk -f link\ver_128k_2.4.lk
