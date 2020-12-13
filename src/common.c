e9204b4441ff0cf4981912b843dc555b47cf9379
Georg Icking-Konert <icking@onlinehome.de>
2020-12-13 19:37:26 +0100

    fixed bitmask bugs
    - use of bitshift (bug)
    - explicit typecast (avoid warning)

--- src/common.c
+++ src/common.c
@@ -7,26 +7,26 @@
 
 void flash_erase_enable(void) {
 	// Enable flash block erasure.
-	FLASH_CR2 = (1 << FLASH_CR2_ERASE);
-	FLASH_NCR2 = ~(1 << FLASH_NCR2_NERASE);
+	FLASH_CR2  = (uint8_t) (1 << FLASH_CR2_ERASE);
+	FLASH_NCR2 = (uint8_t) ~(1 << FLASH_NCR2_NERASE);
 }
 
 void flash_block_prg_enable(void) {
 	// Enable flash block writing.
-	FLASH_CR2 = FLASH_CR2_PRG;
-	FLASH_NCR2 = ~FLASH_NCR2_NPRG;
+	FLASH_CR2  = (uint8_t) (1 << FLASH_CR2_PRG);
+	FLASH_NCR2 = (uint8_t) ~(1 << FLASH_NCR2_NPRG);
 }
 
 void flash_block_prg_option_wr_enable(void) {
 	// Enable writing of option bytes in addition to block writing.
-	FLASH_CR2 = (FLASH_CR2_PRG | FLASH_CR2_OPT);
-	FLASH_NCR2 = ~(FLASH_NCR2_NPRG | FLASH_NCR2_NOPT);
+	FLASH_CR2  = (uint8_t) ((1 << FLASH_CR2_PRG) | (1 << FLASH_CR2_OPT));
+	FLASH_NCR2 = (uint8_t) ~((1 << FLASH_NCR2_NPRG) | (1 << FLASH_NCR2_NOPT));
 }
 
 void flash_prg_wait(uint8_t *prot_flag) {
 	// TODO: earliest versions (128K v2.0 and 32K v1.0) of RAM routines do not
 	// check WR_PG_DIS, and consequently do not have a 'return' value.
-	
+
 	if(FLASH_IAPSR & (1 << FLASH_IAPSR_WR_PG_DIS)) {
 		// Failed, attempted to erase a protected area; set a flag?
 		*prot_flag |= (1 << 0);
